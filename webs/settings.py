4e754b4681ae4d9d9285fec824cc799e9c9191c2
A-Scode <73399909+A-Scode@users.noreply.github.com>
2021-05-07 01:10:16 -0700

    Update settings.py

--- webs/settings.py
+++ webs/settings.py
@@ -142,7 +142,7 @@ CHANNEL_LAYERS = {
 }
 
 import django_heroku
-django_heroku.settings(locals(), logging = True)
+django_heroku.settings(locals(), logging = False)
 
 LOGGING = {
     'version': 1,
