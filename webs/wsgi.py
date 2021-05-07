763767aa9256923aa406e3fe0344331ef7efd765
A-Scode <73399909+A-Scode@users.noreply.github.com>
2021-05-07 06:22:03 -0700

    Update wsgi.py

--- webs/wsgi.py
+++ webs/wsgi.py
@@ -11,6 +11,25 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter ,URLRouter
+from channels.auth import AuthMiddlewareStack
+from . import routing
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webs.settings')
 
-application = get_wsgi_application()
+application = ProtocolTypeRouter({
+    'http': get_wsgi_application(),
+
+    'websocket':AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+    'wss':AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    )
+
+})
